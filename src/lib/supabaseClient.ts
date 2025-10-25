2e2af49282b9392d0571dd107f0dc058e71b79e8
junye0l <junyeolkim00@gmail.com>
2025-10-25 17:51:42 +0900

    chore : superbase 연동 테스트

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,29 @@
+// src/lib/supabaseClient.ts
+import { cookies } from "next/headers";
+import { createBrowserClient, createServerClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// CSR/클라이언트 컴포넌트용
+export const createSupabaseBrowserClient = () =>
+  createBrowserClient(supabaseUrl, supabaseAnonKey);
+
+// SSR/Route Handler/Server Action용
+export const createSupabaseServerClient = async () => {
+  const cookieStore = await cookies();
+
+  return createServerClient(supabaseUrl, supabaseAnonKey, {
+    cookies: {
+      get(name) {
+        return cookieStore.get(name)?.value;
+      },
+      set(name, value, options) {
+        cookieStore.set({ name, value, ...options });
+      },
+      remove(name, options) {
+        cookieStore.delete({ name, ...options });
+      },
+    },
+  });
+};
